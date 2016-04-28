aca6be95377ad9784d6c25f184b9f7eeca834a5b
Lev Ostanin <levkus@gmail.com>
2016-04-28 14:00:21 +0300

    send to csssr

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -1,77 +1,10 @@
 import $ from 'jquery';
+import jsSlider from '../blocks/javascript-slider/javascript-slider';
 
-function jsSlider() {
-	const $container = $('#slider-container');
-	const $pointer = $('#pointer');
-	const $ghost = $('#pointer-ghost');
-	const $selectors = $('.javascript-slider__level-selector');
-	const $pointerValue = $('#pointer-value');
-	let sliderWidth = $container.width();
-	let offsetLeft = $container.offset().left;
-	let offsetRight = offsetLeft + sliderWidth;
-	let percent = sliderWidth / 100;
-	let selectedPercent;
-
-	// Вычисляем текущий процент и записываем его в selectedPercent
-	function getPercent() {
-		selectedPercent = Math.max(0, Math.min(($pointer.offset().left - offsetLeft + $pointer.width() / 2) / percent, 100)).toFixed(1);
-		console.log('JS Skill: ' + selectedPercent + '%');
-		$pointerValue.html(selectedPercent + '%');
-		return selectedPercent;
-	}
-
-	// ставим pointer в начальное положение через атрибут data-percent
-	function init() {
-		const value = Math.max(0, Math.min($pointer.attr('data-percent'), 100));
-		$pointer.animate({left: value * percent}, 0, getPercent);
-	}
-
-	// Обновляем переменные при изменении размера окна и ставим pointer
-	// в нужное положение в соответствии с обновлениями
-	$(window).resize( () => {
-		sliderWidth = $container.width();
-		offsetLeft = $container.offset().left;
-		offsetRight = offsetLeft + sliderWidth;
-		percent = sliderWidth / 100;
-		$pointer.css({left: (percent * selectedPercent)});
-	});
-
-	// Обрабатываем события мыши
-	function handleMouse() {
-		$container.on('mouseenter mouseleave mousemove click', e => {
-			const x = Math.min(Math.max(e.pageX, offsetLeft), offsetRight);
-			const pointerLeft = x - offsetLeft;
-			switch (e.type) {
-				case 'mouseenter':
-					$ghost.css('opacity', 0.5);
-					break;
-				case 'mouseleave':
-					$ghost.css('opacity', 0);
-					break;
-				case 'mousemove':
-					$ghost.css({left: pointerLeft});
-					break;
-				case 'click':
-					$pointer.animate({left: pointerLeft}, 150, getPercent);
-					break;
-			}
-		});
-	}
-
-	// Обарабатываем нажатие на селекторы через атрибут data-percent
-	function handleSelectors() {
-		$selectors.on('click', function () {
-			const value = Math.max(0, Math.min($(this).attr('data-percent'), 100));
-			$pointer.animate({left: value * percent}, 150, getPercent);
-		});
-	}
-
-	// Выносим getPercent наружу, вызывать через jsSlider.getPercent();
-	jsSlider.getPercent = getPercent;
-
-	init();
-	handleMouse();
-	handleSelectors();
-}
-
-jsSlider();
+jsSlider(
+	$('#slider-container'),
+	$('#pointer'),
+	$('#pointer-ghost'),
+	$('.javascript-slider__level-selector'),
+	$('#pointer-value')
+);
